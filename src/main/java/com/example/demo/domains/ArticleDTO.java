8b2997742954523a2dcfcf3d008d68441ff73694
bmar3315 <isr0618@naver.com>
2020-12-07 10:38:48 +0900

    12/07 10

--- src/main/java/com/example/demo/domains/ArticleDTO.java
+++ src/main/java/com/example/demo/domains/ArticleDTO.java
@@ -2,7 +2,10 @@ package com.example.demo.domains;
 
 import org.springframework.stereotype.Component;
 
-@Component
+import lombok.Getter;
+import lombok.Setter;
+
+@Component @Getter @Setter
 public class ArticleDTO {
-    
+    private String artNum, writerId, title, content, regDate ;
 }
